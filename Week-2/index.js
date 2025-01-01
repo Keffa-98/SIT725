497534d95738f6b8f25e209e7fb41deafe2a9f64
Keffa Kirwa <s223664997@deakin.edu.au>
2025-01-02 01:42:24 +1100

    week 2 part 2

--- Week-2/index.js
+++ Week-2/index.js
@@ -1,9 +1,24 @@
 const express = require("express")
 const app = express()
+
+
 const port = process.env.PORT || 3000;
 
+
+app.get('/add',(req,res)=>{
+    const num1 = parseInt(req.query.num1);
+    const num2 = parseInt(req.query.num2);
+    const result = addTwoNumbers(num1,num2)
+    return res.status(200).json({
+        result
+    })
+})
 
 
 app.listen(port,()=>{
     console.log(`App listening on port ${port}`)
 })
+
+const addTwoNumbers = (num1,num2)=>{
+    return num1+num2
+}
