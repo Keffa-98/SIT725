29c9987505438b7b4f1442be2cbc0ae426152d6e
Keffa Kirwa <s223664997@deakin.edu.au>
2025-01-02 02:07:46 +1100

    added query validation

--- Week-2/index.js
+++ Week-2/index.js
@@ -1,24 +1,27 @@
-const express = require("express")
-const app = express()
-
+const express = require("express");
+const app = express();
 
 const port = process.env.PORT || 3000;
 
-
-app.get('/add',(req,res)=>{
+app.get("/add", (req, res) => {
+  if (req.query.num1 && req.query.num2) {
     const num1 = parseInt(req.query.num1);
     const num2 = parseInt(req.query.num2);
-    const result = addTwoNumbers(num1,num2)
+    const result = addTwoNumbers(num1, num2);
     return res.status(200).json({
-        result
+      result,
+    });
+  } else {
+    return res.status(400).json({
+        "error":"provide num1 and num2 in request query"
     })
-})
+  }
+});
 
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
 
-app.listen(port,()=>{
-    console.log(`App listening on port ${port}`)
-})
-
-const addTwoNumbers = (num1,num2)=>{
-    return num1+num2
-}
+const addTwoNumbers = (num1, num2) => {
+  return num1 + num2;
+};
