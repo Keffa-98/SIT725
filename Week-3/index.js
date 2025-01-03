a06aa0d05642f14cab755f81e0259f295e63a160
Keffa Kirwa <s223664997@deakin.edu.au>
2025-01-03 23:28:40 +1100

    week 3: bootstrapping express app

---
+++ Week-3/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
